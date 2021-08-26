1b6f84f0643824c56e277dc64aa924300c91d035
Abraham Chavez <abraham86.201@gmail.com>
2021-08-25 23:53:28 -0500

    ActionTypes added

--- front/src/redux/actions/itemActions.js
+++ front/src/redux/actions/itemActions.js
@@ -1,6 +1,8 @@
+import * as types from './actionTypes'
+
 export function createItem(item){
     return {
-        type : "CREATE_ITEM",
+        type : types.CREATE_COURSE,
         item : item
     }
 }
--- front/src/redux/reducers/itemReducer.js
+++ front/src/redux/reducers/itemReducer.js
@@ -1,6 +1,7 @@
+import * as types from '../actions/actionTypes';
 export default function  itemReducer(state = [], action) {
     switch(action.type){
-        case "CREATE_ITEM":
+        case types.CREATE_COURSE:
             return [...state, { ...action.item}];
         default:
             return state;
